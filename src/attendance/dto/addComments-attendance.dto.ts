b70f663e3112cb3ffae8ca215d711e19f51866a8
WilberthGamboa <wgcampos15@gmail.com>
2023-11-09 09:55:33 -0600

    se agrega documentacion dto

--- src/attendance/dto/addComments-attendance.dto.ts
+++ src/attendance/dto/addComments-attendance.dto.ts
@@ -1,12 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString, IsUUID } from "class-validator";
 import { id } from "date-fns/locale";
 
 export class AddCommentDto{
-
+    @ApiProperty(
+        {
+            description:'Id del attendance'
+        }
+    )
     @IsUUID()
     @IsNotEmpty()
     idAttendance:string;
-
+    @ApiProperty(
+        {
+            description:'Comentario a agregar attendance'
+        }
+    )
     @IsString()
     @IsNotEmpty()
     comment:string;
