fe3641d2bdbe34b1ba1b47843d803bbf8c78fd71
WilberthGamboa <wgcampos15@gmail.com>
2023-12-02 16:14:48 -0600

    se agregan los imports

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { AttendanceModule } from 'src/attendance/attendance.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports:[
+    AuthModule,
+    AttendanceModule,
+    
+  ]
 })
 export class SeedModule {
 
